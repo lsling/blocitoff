06ac0a192496450a57a28088ea0c264beb932cba
Laura Slingerland <laura.sling@gmail.com>
2017-10-03 23:00:44 -0400

    UI cleanup

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,24 +1,23 @@
 (function() {
-    function HomeCtrl($scope, $uibModal, Task) {
+  function HomeCtrl($scope, $uibModal, Task) {
 
-      this.tasks = Task.all;
+    this.tasks = Task.all;
 
-      this.open = function() {
-            var modalInstance = $uibModal.open({
-                animation: true,
-                templateUrl: '/templates/task-modal.html',
-                controller: 'ModalCtrl as modal',
-                windowClass: 'modal-window'
-            });
+    this.open = function() {
+      var modalInstance = $uibModal.open({
+        animation: true,
+        templateUrl: '/templates/task-modal.html',
+        controller: 'ModalCtrl as modal',
+        windowClass: 'modal-window'
+      });
 
-            modalInstance.result.then(function(name) {
-				          Task.createTask(name);
-			      });
-        };
+      modalInstance.result.then(function(name) {
+        Task.createTask(name);
+      });
+    };    
+  }
 
-    }
-
-    angular
-        .module('blocItOff')
-        .controller('HomeCtrl', ['$scope', '$uibModal', 'Task', HomeCtrl]);
+  angular
+    .module('blocItOff')
+    .controller('HomeCtrl', ['$scope', '$uibModal', 'Task', HomeCtrl]);
 })();
